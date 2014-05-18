4706d2794ad80853ca5e229814c323b003cfc7ca
Juan Manuel Schillaci <jmschillaci@gmail.com>
2014-05-18 11:10:47 -0300

    Added focus script with useful shortcuts

---
+++ .i3/i3-wrapper.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#
+# i3-wrapper associates strings supplied by i3-input with programs.
+#
+# It is inspired by the configurable keyboard shortcuts for bookmarks
+# in web browsers such as Firefox and Google Chrome.
+#
+# Type a string and the corresponding program is brought into focus;
+# if it is already running focus moves to its associate workspace,
+# otherwise it is launched in the current workspace.
+#
+# Take care to match long strings before short strings beginning with
+# identical substrings.
+#
+# The easiest match to use is 'instance' because the instance sring
+# usually matches the program name. Using 'title' is complicated by
+# the need to enclose the matched substring in quotes but it can be a
+# good choice in some cases.
+#
+# Contains at least one bashism: (link deleted so I can post)
+#
+
+focus ()
+{
+    case $1 in
+        (instance)
+        W=$(xdotool search --classname "$2" | head -1)
+        ;;
+        (class)
+        W=$(xdotool search --class "$2" | head -1)
+        ;;
+        (title)
+        W=$(xdotool search --name "$2" | head -1)
+        ;;
+        (*)
+        W=''
+        ;;
+    esac
+    if [ -z "$W" ]; then
+        # Launch the program. 
+        eval ${@:3} &  # bash
+    else
+        # Focus the window.
+        # Used 'sed' to escape any whitepace to suit 'i3-msg'.
+        i3-msg "[$1=$(echo $2 | sed s/\\x20/\\\\x20/g)] focus"
+    fi
+}
+
+start ()
+{
+    case "$@" in
+        (ff)
+        focus title Firefox firefox
+        ;;
+        (tra)
+        focus title Transmission transmission-gtk
+        ;;
+        (ter)
+        focus instance terminator terminator
+        ;;
+        (vim)
+        focus title VIM terminator -x vim
+        ;;
+        (na)
+        focus instance nautilus nautilus
+        ;;
+        (calc)
+        focus title Calculator xcalc
+        ;;
+        (wr)
+        focus title 'LibreOffice Writer' libreoffice --writer
+        ;;
+        (pp)
+        focus title 'LibreOffice Impress' libreoffice --impress
+        ;;
+        (ss)
+        focus title 'LibreOffice Calc' libreoffice --calc
+        ;;
+        (vc|vlc)
+        focus instance vlc vlc
+        ;;
+        (sk|sky)
+        focus instance skype skype
+        ;;
+        (*)
+        "$@" &
+        ;;
+    esac
+}
+
+start "$@"
+
+#
+# Done.
+#
