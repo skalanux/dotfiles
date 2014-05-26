9febb2ff7155ab89ae5740d02ed8d236a5a852d6
Juan Manuel Schillaci <jmschillaci@gmail.com>
2014-05-26 08:33:00 -0300

    Added emacs.d modules

---
+++ .emacs.d/elpa/epc-0.1.1/epc-autoloads.el
@@ -0,0 +1,18 @@
+;;; epc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("epc-pkg.el" "epc.el") (21373 15095 781288
+;;;;;;  720000))
+
+;;;***
+
+(provide 'epc-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; epc-autoloads.el ends here
